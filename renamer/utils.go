9feaada20b34ab56ccc04589d39b5e30cef6785a
owen <qcgzxw.com@gmail.com>
2022-02-13 01:02:30 +0800

    fix bug #5

--- renamer/utils.go
+++ renamer/utils.go
@@ -1,9 +1,11 @@
 package renamer
 
 import (
+	"fmt"
 	"os"
 	"path"
 	"path/filepath"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -92,5 +94,49 @@ func OsRename(oldPath, newPath string) error {
 			delete(invalidNfoPath, oldPath)
 		}
 	}
+	if _, err := os.Stat(oldPath); err != nil {
+		// 旧文件不存在
+		return err
+	}
+	if info, err := os.Stat(newPath); err == nil {
+		if info.IsDir() {
+			return nil
+		}
+		// 已存在文件 适配多版本
+		newPath = genMultiVersionName(oldPath, newPath)
+	}
 	return os.Rename(oldPath, newPath)
+}
+
+func genMultiVersionName(oldPath, newPath string) (s string) {
+	ext := filepath.Ext(oldPath)
+	s = newPath[:len(newPath)-len(ext)]
+	if specials := getMovieSpecialInfo(GetFileName(oldPath)); len(specials) > 0 {
+		s += " - " + strings.Join(specials, ".")
+	}
+	for i := 1; i > 0; i++ {
+		if _, err := os.Stat(s); err != nil {
+			break
+		}
+		s = newPath + fmt.Sprintf(" (%d)", i)
+	}
+	return s + ext
+}
+
+func getMovieSpecialInfo(filePath string) (specials []string) {
+	// https://en.wikipedia.org/wiki/Pirated_movie_release_types
+	piratedMovieReleaseTypes := []string{"CAM\\-Rip", "CAM", "HDCAM", "TS", "HDTS", "TELESYNC", "PDVD", "PreDVDRip", "WP", "WORKPRINT", "TC", "HDTC", "TELECINE", "PPV", "PPVRip", "SCR", "SCREENER", "DVDSCR", "DVDSCREENER", "BDSCR", "WEBSCREENER", "DDC", "R5", "R5\\.LINE", "R5\\.AC3\\.5\\.1\\.HQ", "DVDRip", "DVDMux", "DVDR", "DVD\\-Full", "Full\\-Rip", "ISO rip", "lossless rip", "untouched rip", "DVD\\-5", "DVD\\-9", "DSR", "DSRip", "SATRip", "DTHRip", "DVBRip", "HDTV", "PDTV", "DTVRip", "TVRip", "HDTVRip", "VODRip", "VODR", "HC", "HD\\-Rip", "WEB\\-Cap", "WEBCAP", "WEB\\sCap", "HDRip", "WEB\\-DLRip", "WEBRip", "WEB\\sRip", "WEB\\-Rip", "WEBDL", "WEB\\sDL", "WEB\\-DL", "WEB", "WEBRip", "Blu\\-Ray", "BluRay", "BLURAY", "BDRip", "BRip", "BRRip", "BDR", "BD25", "BD50", "BD5", "BD9", "BDMV", "BDISO", "COMPLETE\\.BLURAY"}
+	// https://en.wikipedia.org/wiki/Graphics_display_resolution
+	resolutionList := []string{"240i", "288i", "480i", "576i", "480p", "576p", "720p", "1080i", "1080p", "2160p", "4320p", "nHD", "qHD", "HD", "HD+", "FHD", "DCI\\s2K", "QHD", "QHD+", "4K\\sUHD", "DCI\\s4K", "5K", "8K\\sUHD", "16K"}
+	// https://en.wikipedia.org/wiki/Video_coding_format
+	videoCodecList := []string{"divx", "dv", "h\\.120", "h\\.261", "h\\.262", "h\\.263", "h\\.264", "h\\.265", "h\\.266", "h120", "h261", "h262", "h263", "h264", "h265", "h266", "hevc", "mj2", "mpeg1", "mpeg2", "mpeg4", "mpegh", "mpeg\\-1", "mpeg\\-2", "mpeg\\-4", "mpeg\\-h", "vc\\-1", "vcc", "x264", "x265", "vc1"}
+
+	list := [][]string{resolutionList, piratedMovieReleaseTypes, videoCodecList}
+	for _, r := range list {
+		regex := regexp.MustCompile("(?i)\\W(" + strings.Join(r, "|") + ")\\W")
+		if specialMatch := regex.FindStringSubmatch(filePath); len(specialMatch) > 0 && specialMatch[1] != "" {
+			specials = append(specials, specialMatch[1])
+		}
+	}
+	return
 }
